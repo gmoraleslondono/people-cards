d03af622985a3f356f08d1827f9c267cb04e8b50
Gloria Morales <gmoraleslondono@gmail.com>
2021-01-07 21:55:40 +0100

    Add data from API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users: [],
+      pageNum: 1,
+    }
+  }
+
+  componentDidMount() {
+    const url = `https://reqres.in/api/users?page=${this.state.pageNum}`;
+
+    fetch(url)
+      .then(response => response.json())
+      // .then(response => console.log('response', response))
+      .then(response => this.setState({ users: response.data}))
+  }
+
   render() {
+    const { users } = this.state;
+
     return (
-      <p>This is my react app</p>
+      <div>
+        <p>People Cards</p>
+          {users.map((user) => (
+            <div>
+              <img src={user.avatar} alt="Person image"/>
+              <p key={user.id}>{user.email}</p>
+            </div>
+          ))}
+      </div>
     )
   }
 }
